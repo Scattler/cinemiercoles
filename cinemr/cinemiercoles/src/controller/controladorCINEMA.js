a20642c5077d117bfc5665f7541615d0470e1c68
Scattler <moises6xp@gmail.com>
2022-08-25 18:06:34 -0500

    se inicio la creacion del arreglo

--- cinemr/cinemiercoles/src/controller/controladorCINEMA.js
+++ cinemr/cinemiercoles/src/controller/controladorCINEMA.js
@@ -1,10 +1,41 @@
-//declarando arreglos en JS
+let peliculas=[
+    {
+        nombre:"avatar",
+        genero:"Ciencia ficción",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/avatar.png?alt=media&token=4775b8fa-5e9c-42bb-9ae3-f0f0bf010f0d",
+        sinopsis:"loremjasjsadjsajsdajsajsdajsda",
+        clasificacion:"+18",
+        idioma:"En subtitulado"
+    },
+    {
+        nombre:"La Bestia",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/la%20bestia.jpg?alt=media&token=b46a53b9-3af7-4fc2-9c7e-966ec628f719",
+        sinopsis:"loremjasjsadjsajsdajsajsdajsda",
+        clasificacion:"+18",
+        idioma:"En subtitulado"
+    },
+    {
 
-//Un arreglo es una variable especial que me permite almacenar multiples datos en una sola variable
+    },
+    {
 
-let numeros=[5,8,12,10]
-let nombres=Array("juan","sara","carlos","laura")
-console.log(nombres[2])
+    },
+   
+]
 
 
+//RECORRIENDO UN ARREGLO EN JS
+peliculas.forEach(function(pelicula){
+    //console.log(pelicula)
+    console.log(pelicula.nombre)
+    console.log(pelicula.genero)
+    console.log(pelicula.duracion)
+    console.log(pelicula.poster)
+    console.log(pelicula.sinopsis)
+    console.log(pelicula.clasificacion)
+    console.log(pelicula.idioma)
+})
 
