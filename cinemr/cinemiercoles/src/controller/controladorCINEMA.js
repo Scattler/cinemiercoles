50034b1efd185c0aed32c6344670172899c20f45
Scattler <moises6xp@gmail.com>
2022-08-25 17:31:43 -0500

    se agrega el proyecto cinemiercoles

---
+++ cinemr/cinemiercoles/src/controller/controladorCINEMA.js
@@ -0,0 +1,19 @@
+//declarando arreglos en JS
+
+//Un arreglo es una variable especial que me permite almacenar multiples datos en una sola variable
+
+let numeros=[5,8,12,10]
+let nombres=Array("juan","sara","carlos","laura")
+console.log(nombres[2])
+
+
+
+let persona={
+    nombre:"Moises",
+    profesion:"Ingeniero",
+    edad:24,
+    HinchaDelMejor:true,
+    materiasDictadas:["web2","avanzada","nueva"]
+}
+
+console.log(persona.nombre)
