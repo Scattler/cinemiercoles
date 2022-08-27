d440cdb39730232e1cddd2dfab7a6dbcb50ea6e5
Scattler <moises6xp@gmail.com>
2022-08-27 15:20:18 -0500

    Se agregaron todas las peliculas

--- cinemr/cinemiercoles/src/controller/controladorCINEMA.js
+++ cinemr/cinemiercoles/src/controller/controladorCINEMA.js
@@ -1,29 +1,97 @@
 let peliculas=[
     {
-        nombre:"avatar",
-        genero:"Ciencia ficción",
+        nombre:"Minamata",
+        genero:"Drama",
         duracion:220,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/avatar.png?alt=media&token=4775b8fa-5e9c-42bb-9ae3-f0f0bf010f0d",
-        sinopsis:"loremjasjsadjsajsdajsajsdajsda",
-        clasificacion:"+18",
-        idioma:"En subtitulado"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F1728161.jpg-r_1280_720-f_jpg-q_x-xxyxx.jpg?alt=media&token=b58e572b-9af3-4a8b-a1b0-a85398b4b9e8",
+        sinopsis:"Nueva York, 1971. Tras sus celebrados días como uno de los fotoperiodistas más venerados de la Segunda Guerra Mundial, W. Eugene Smith se siente desconectado de la sociedad y de su carrera. La revista Life lo envía a la ciudad costera japonesa de Minamata, cuya población ha sido devastada por el envenenamiento por mercurio, resultado de décadas de negligencia industrial. Allí, Smith se sumerge en la comunidad y recupera la ilusión por su profesión y con su cámara capta las imágenes que darán la vuelta al mundo.",
+        clasificacion:"PG-13",
+        idioma:"EN Subtitulado"
     },
     {
-        nombre:"La Bestia",
-        genero:"suspenso",
-        duracion:120,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/la%20bestia.jpg?alt=media&token=b46a53b9-3af7-4fc2-9c7e-966ec628f719",
-        sinopsis:"loremjasjsadjsajsdajsajsdajsda",
-        clasificacion:"+18",
-        idioma:"En subtitulado"
+        nombre:"The Batman",
+        genero:"Superheroes/Acción",
+        duracion:155,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F2022126184619_1.jpg?alt=media&token=e9cd7630-337e-472e-8f2a-db19873049ae",
+        sinopsis:"Después de dos años acechando por las calles de la ciudad como Batman (Robert Pattinson), e infundiendo miedo en las mentes perversas de los criminales, Bruce Wayne está sumido en las profundidades de las sombras de Gotham City. Este vigilante solitario cuenta con pocos aliados de confianza y eso le ha llevado a convertirse en la única encarnación de la venganza entre sus conciudadanos. Cuando un asesino apunta a la élite de Gotham con una serie de maquinaciones sádicas, un rastro de pistas crípticas lleva a Batman a realizar una investigación en el inframundo.",
+        clasificacion:"B-15",
+        idioma:"EN/ESP Doblado "
     },
     {
+        nombre:"A Ciegas",
+        genero:"Suspenso/Thriller",
+        duracion:115,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F38883-774385-20220302072803.jpg?alt=media&token=a4bb7c16-670d-45e8-9e2e-0cd9914f1bc3",
+        sinopsis:"Sophie es una joven ciega que se dedica a cuidar casas mientras sus dueños están fuera. En un trabajo de último minuto descubre que la casa está siendo invadida por ladrones que buscan algo en específico. Su único medio de defensa es una aplicación llamada A Ciegas, que permite que voluntarios vean por ella a través de la cámara. A través de la aplicación una veterana del ejército, Kelly, ayuda a Sophie a tratar de sobrevivir la noche.",
+        clasificacion:"C",
+        idioma:"EN Subtitulado"
 
     },
     {
+        nombre:"Uncharted: Fuera del mapa",
+        genero:"Acción/Aventura",
+        duracion:143,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F3XBU6AMRYVHH3FJTYBIC5CRQ5Y.jpg?alt=media&token=86f2b7c0-d572-44e9-8924-65d52c3343cb",
+        sinopsis:"Basada en una de las series de videojuegos más vendidas y aclamadas por la crítica de todos los tiempos, Uncharted presenta a un joven, astuto y carismático, Nathan Drake (Tom Holland) en su primera aventura como cazatesoros con su ingenioso compañero Victor “Sully” Sullivan (Mark Wahlberg). En una aventura de acción que se extiende por todo el mundo, ambos se embarcan en una peligrosa búsqueda de “el mayor tesoro nunca antes encontrado” al tiempo que rastrean las claves que les podrían conducir al hermano de Nathan, perdido hace ya mucho tiempo.",
+        clasificacion:"PG-13",
+        idioma:"ESP Subtitulado"
 
     },
-   
+
+    {
+        nombre:"Bad Guys",
+        genero:"Animada/Comedia",
+        duracion:145,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FMV5BODI2ZGUwNzctNTM2My00NzE4LWFjNjYtN2YxMDk4ODAwZWZiXkEyXkFqcGdeQXVyMTMzODk3NDU0._V1_.jpg?alt=media&token=112d6de5-5e6b-4ea2-ad42-7430f7aa5982",
+        sinopsis:"En la nueva comedia de acción de DreamWorks Animation, basada en la exitosa serie de libros, una banda de animales exitosos en el crimen está a punto de afrontar su mayor desafío-convertirse en ciudadanos modelo. Nunca hubo cinco amigos tan infames como Los Tipos Malos-el preciso carterista Sr. Lobo, el ladrón que lo ha visto todo Sr.Serpiente, el tranquilo maestro del disfraz Sr.Tiburón, el musculoso Sr.Piraña y la hacker experta Srita. Tarántula. Pero cuando, después de años de numerosos atracos y de ser los villanos más buscados del mundo, la pandilla es finalmente capturada, el Sr. Lobo rompe un trato (que no tenía intención de mantener) para salvarlos a todos de prisión: Los Tipos Malos serán buenos.",
+        clasificacion:"SC",
+        idioma:"ESP Doblado"
+    },
+    {
+        nombre:"La peor persona del mundo",
+        genero:"Cine Romántico",
+        duracion:221,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F3kFMOV2R45Ldg4yCI0moJUJYdL7.jpg?alt=media&token=2cdef3a2-5bc1-4871-83a5-7b77da48f793",
+        sinopsis:"Julie va a cumplir los treinta y su vida es un desastre existencial. Ya ha desperdiciado parte de su talento y su novio Aksel, un exitoso novelista gráfico mayor que ella, la presiona para que contenga su energía creativa y siente la cabeza. Una noche se cuela en una fiesta y conoce al joven y encantador Eivind. Tardará poco en romper con Aksel y embarcarse en una nueva relación con la esperanza de que su vida adquiera una nueva perspectiva. Sin embargo, tendrá que darse cuenta de que ya es demasiado tarde para ciertas opciones vitales.",
+        clasificacion:"SC",
+        idioma:"ESP"
+    },
+    {
+        nombre:"Flow Calle",
+        genero:"Drama/Música",
+        duracion:163,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FEL_FRENTE_02022005021011019054_Ventana.jpg?alt=media&token=a4b60fb5-e619-4560-a8dc-d2ade28236fc",
+        sinopsis:"“Flow Calle” es una historia que busca exponer la realidad que se vive en cualquier barrio de Latinoamérica, donde jóvenes talentosos con historias increíbles que contar, destacando la cultura luchadora del dominicano, mostrará lo más hermoso de nuestros barrios con mensajes positivos y cargado de valores",
+        clasificacion:"SC",
+        idioma:"ESP"
+    },
+    {
+        nombre:"Animales fantásticos: Los secretos de Dumbledore",
+        genero:"Fantasía",
+        duracion:176,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FFNQYjOUXEAAIVYy.jpg?alt=media&token=7a4ad084-d8dc-4f1f-847f-6027d5c41bf2",
+        sinopsis:"En Animales Fantásticos: Los Secretos de Dumbledore el malvado y poderoso mago Grindelwald sigue buscando adeptos a su causa, pero esta vez se transportará a todos los rincones y mundos mágicos existentes para obtener lo que quiere. Por otra parte, Dumbledore unirá a su propio ejército formado por Newt y Theseus Scamander, Jacob, entre otros, para poner fin a la guerra que está a punto de comenzar Grindelwald en su nombre. Mientras encuentran aliados y la manera de poner fin a esta oscura edad, el pasado de Dumbledore irá cada vez tomando más presencia en la historia entre él y su antiguo amigo Grindelwald, con quien le unen otros grandes lazos más allá de la enemistad.",
+        clasificacion:"PG-13",
+        idioma:"EN subtitulado"
+    },
+    {
+        nombre:"Morbius",
+        genero:"Acción/Ciencia ficción ",
+        duracion:225,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2Fposter-morbius-clip.jpg?alt=media&token=02b865dc-6df6-4ca2-b95c-0db1f4852f6b",
+        sinopsis:"El Doctor Michael Morbius (Jared Leto) es un bioquímico que sufre una extraña enfermedad en la sangre. Al intentar curarse y dar una respuesta a su trastorno se infecta sin darse cuenta con una forma de vampirismo. Tras la cura, Michael se siente más vivo que nunca y adquiere varios dones como fuerza y velocidad, además de una necesidad irresistible de consumir sangre. Trágicamente convertido en un imperfecto antihéroe, el Doctor Morbius tendrá una última oportunidad, pero sin saber a qué precio.",
+        clasificacion:"B-15",
+        idioma:"EN Subtitulado"
+    },
+    {
+        nombre:"Scream",
+        genero:"Terror",
+        duracion:192,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2Fscream-scream-5-2497975.webp?alt=media&token=7d25f8eb-5fd9-482d-9bde-cc919a113d6d",
+        sinopsis:"La pesadilla ha regresado. Veinticinco años después de que tuvieran lugar aquellos brutales asesinatos en el pueblo de Woodsboro, California, y conmocionaran a sus habitantes, un nuevo asesino ha regresado. Usando el mismo rostro que Ghostface, el nuevo agresor va a comenzar a atacar a un grupo de adolescentes para sacar a la luz secretos del mortífero pasado del pueblo.",
+        clasificacion:"C",
+        idioma:"ESP Doblado"
+    }
 ]
 
 
