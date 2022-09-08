2df04624932680fe330976dcbbd562e19e90ea64
505 <505@academicos.cesde.edu.co>
2022-09-07 19:46:59 -0500

    Se entrega proyecto

--- cinemr/cinemiercoles/src/controller/controladorCINEMA.js
+++ cinemr/cinemiercoles/src/controller/controladorCINEMA.js
@@ -6,7 +6,10 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F1728161.jpg-r_1280_720-f_jpg-q_x-xxyxx.jpg?alt=media&token=b58e572b-9af3-4a8b-a1b0-a85398b4b9e8",
         sinopsis:"Nueva York, 1971. Tras sus celebrados días como uno de los fotoperiodistas más venerados de la Segunda Guerra Mundial, W. Eugene Smith se siente desconectado de la sociedad y de su carrera. La revista Life lo envía a la ciudad costera japonesa de Minamata, cuya población ha sido devastada por el envenenamiento por mercurio, resultado de décadas de negligencia industrial. Allí, Smith se sumerge en la comunidad y recupera la ilusión por su profesión y con su cámara capta las imágenes que darán la vuelta al mundo.",
         clasificacion:"PG-13",
-        idioma:"EN Subtitulado"
+        idioma:"EN Subtitulado",
+        director: "Andrew Levitas",
+        actores: "Johnny Depp, Bill Nighty, Hiroyuki Sanada"
+
     },
     {
         nombre:"The Batman",
@@ -15,7 +18,9 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F2022126184619_1.jpg?alt=media&token=e9cd7630-337e-472e-8f2a-db19873049ae",
         sinopsis:"Después de dos años acechando por las calles de la ciudad como Batman (Robert Pattinson), e infundiendo miedo en las mentes perversas de los criminales, Bruce Wayne está sumido en las profundidades de las sombras de Gotham City. Este vigilante solitario cuenta con pocos aliados de confianza y eso le ha llevado a convertirse en la única encarnación de la venganza entre sus conciudadanos. Cuando un asesino apunta a la élite de Gotham con una serie de maquinaciones sádicas, un rastro de pistas crípticas lleva a Batman a realizar una investigación en el inframundo.",
         clasificacion:"B-15",
-        idioma:"EN/ESP Doblado "
+        idioma:"EN/ESP Doblado ",
+        director:"Michael Giacchino",
+        actores: "Robert Pattinson, Zoe Kravitz, Paul Dano"
     },
     {
         nombre:"A Ciegas",
@@ -24,7 +29,9 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F38883-774385-20220302072803.jpg?alt=media&token=a4bb7c16-670d-45e8-9e2e-0cd9914f1bc3",
         sinopsis:"Sophie es una joven ciega que se dedica a cuidar casas mientras sus dueños están fuera. En un trabajo de último minuto descubre que la casa está siendo invadida por ladrones que buscan algo en específico. Su único medio de defensa es una aplicación llamada A Ciegas, que permite que voluntarios vean por ella a través de la cámara. A través de la aplicación una veterana del ejército, Kelly, ayuda a Sophie a tratar de sobrevivir la noche.",
         clasificacion:"C",
-        idioma:"EN Subtitulado"
+        idioma:"EN Subtitulado",
+        director: "Susanne Bier",
+        actores:"Sandra Bullock, Trevane Rhodes, John Malkovich"
 
     },
     {
@@ -34,7 +41,9 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F3XBU6AMRYVHH3FJTYBIC5CRQ5Y.jpg?alt=media&token=86f2b7c0-d572-44e9-8924-65d52c3343cb",
         sinopsis:"Basada en una de las series de videojuegos más vendidas y aclamadas por la crítica de todos los tiempos, Uncharted presenta a un joven, astuto y carismático, Nathan Drake (Tom Holland) en su primera aventura como cazatesoros con su ingenioso compañero Victor “Sully” Sullivan (Mark Wahlberg). En una aventura de acción que se extiende por todo el mundo, ambos se embarcan en una peligrosa búsqueda de “el mayor tesoro nunca antes encontrado” al tiempo que rastrean las claves que les podrían conducir al hermano de Nathan, perdido hace ya mucho tiempo.",
         clasificacion:"PG-13",
-        idioma:"ESP Subtitulado"
+        idioma:"ESP Subtitulado",
+        director:"Ruben Fleischer",
+        actores:"Tom Holland, Sophia Ali, Mark Wanberg",
 
     },
 
@@ -45,7 +54,10 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FMV5BODI2ZGUwNzctNTM2My00NzE4LWFjNjYtN2YxMDk4ODAwZWZiXkEyXkFqcGdeQXVyMTMzODk3NDU0._V1_.jpg?alt=media&token=112d6de5-5e6b-4ea2-ad42-7430f7aa5982",
         sinopsis:"En la nueva comedia de acción de DreamWorks Animation, basada en la exitosa serie de libros, una banda de animales exitosos en el crimen está a punto de afrontar su mayor desafío-convertirse en ciudadanos modelo. Nunca hubo cinco amigos tan infames como Los Tipos Malos-el preciso carterista Sr. Lobo, el ladrón que lo ha visto todo Sr.Serpiente, el tranquilo maestro del disfraz Sr.Tiburón, el musculoso Sr.Piraña y la hacker experta Srita. Tarántula. Pero cuando, después de años de numerosos atracos y de ser los villanos más buscados del mundo, la pandilla es finalmente capturada, el Sr. Lobo rompe un trato (que no tenía intención de mantener) para salvarlos a todos de prisión: Los Tipos Malos serán buenos.",
         clasificacion:"SC",
-        idioma:"ESP Doblado"
+        idioma:"ESP Doblado",
+        director:"Pierre Perifel",
+        actores:"Sam Rock, Richard Aoyade, Zazie Beetz",
+
     },
     {
         nombre:"La peor persona del mundo",
@@ -54,7 +66,10 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2F3kFMOV2R45Ldg4yCI0moJUJYdL7.jpg?alt=media&token=2cdef3a2-5bc1-4871-83a5-7b77da48f793",
         sinopsis:"Julie va a cumplir los treinta y su vida es un desastre existencial. Ya ha desperdiciado parte de su talento y su novio Aksel, un exitoso novelista gráfico mayor que ella, la presiona para que contenga su energía creativa y siente la cabeza. Una noche se cuela en una fiesta y conoce al joven y encantador Eivind. Tardará poco en romper con Aksel y embarcarse en una nueva relación con la esperanza de que su vida adquiera una nueva perspectiva. Sin embargo, tendrá que darse cuenta de que ya es demasiado tarde para ciertas opciones vitales.",
         clasificacion:"SC",
-        idioma:"ESP"
+        idioma:"ESP",
+        director:"Rob Marshall",
+        actores:"Maria Grazia, Hans Olvav, Mark Addy",
+
     },
     {
         nombre:"Flow Calle",
@@ -63,7 +78,10 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FEL_FRENTE_02022005021011019054_Ventana.jpg?alt=media&token=a4b60fb5-e619-4560-a8dc-d2ade28236fc",
         sinopsis:"“Flow Calle” es una historia que busca exponer la realidad que se vive en cualquier barrio de Latinoamérica, donde jóvenes talentosos con historias increíbles que contar, destacando la cultura luchadora del dominicano, mostrará lo más hermoso de nuestros barrios con mensajes positivos y cargado de valores",
         clasificacion:"SC",
-        idioma:"ESP"
+        idioma:"ESP",
+        director:"Frank Perozo",
+        actores:"Marta Gonzalez,  Clarisa Molina, Shalim Ortiz",
+
     },
     {
         nombre:"Animales fantásticos: Los secretos de Dumbledore",
@@ -72,7 +90,10 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FFNQYjOUXEAAIVYy.jpg?alt=media&token=7a4ad084-d8dc-4f1f-847f-6027d5c41bf2",
         sinopsis:"En Animales Fantásticos: Los Secretos de Dumbledore el malvado y poderoso mago Grindelwald sigue buscando adeptos a su causa, pero esta vez se transportará a todos los rincones y mundos mágicos existentes para obtener lo que quiere. Por otra parte, Dumbledore unirá a su propio ejército formado por Newt y Theseus Scamander, Jacob, entre otros, para poner fin a la guerra que está a punto de comenzar Grindelwald en su nombre. Mientras encuentran aliados y la manera de poner fin a esta oscura edad, el pasado de Dumbledore irá cada vez tomando más presencia en la historia entre él y su antiguo amigo Grindelwald, con quien le unen otros grandes lazos más allá de la enemistad.",
         clasificacion:"PG-13",
-        idioma:"EN subtitulado"
+        idioma:"EN subtitulado",
+        director:"David Yates",
+        actores:"Eddie Redmayne, Jude Law, Dan Fogler",
+
     },
     {
         nombre:"Morbius",
@@ -81,7 +102,10 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2Fposter-morbius-clip.jpg?alt=media&token=02b865dc-6df6-4ca2-b95c-0db1f4852f6b",
         sinopsis:"El Doctor Michael Morbius (Jared Leto) es un bioquímico que sufre una extraña enfermedad en la sangre. Al intentar curarse y dar una respuesta a su trastorno se infecta sin darse cuenta con una forma de vampirismo. Tras la cura, Michael se siente más vivo que nunca y adquiere varios dones como fuerza y velocidad, además de una necesidad irresistible de consumir sangre. Trágicamente convertido en un imperfecto antihéroe, el Doctor Morbius tendrá una última oportunidad, pero sin saber a qué precio.",
         clasificacion:"B-15",
-        idioma:"EN Subtitulado"
+        idioma:"EN Subtitulado",
+        director:"Daniel Espinosa",
+        actores:"Jared Leto, Matt Smith, Michael Kenton",
+
     },
     {
         nombre:"Scream",
@@ -90,9 +114,75 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2Fscream-scream-5-2497975.webp?alt=media&token=7d25f8eb-5fd9-482d-9bde-cc919a113d6d",
         sinopsis:"La pesadilla ha regresado. Veinticinco años después de que tuvieran lugar aquellos brutales asesinatos en el pueblo de Woodsboro, California, y conmocionaran a sus habitantes, un nuevo asesino ha regresado. Usando el mismo rostro que Ghostface, el nuevo agresor va a comenzar a atacar a un grupo de adolescentes para sacar a la luz secretos del mortífero pasado del pueblo.",
         clasificacion:"C",
-        idioma:"ESP Doblado"
-    }
+        idioma:"ESP Doblado",
+        director:"Kevin Williamson",
+        actores:"Neve Campbell, Courtney Kox, David Arquette",
+
+    },
+
+    {
+        nombre:"Proximamente: LIGHTYEAR",
+        genero:"Ciencia ficción, acción y aventura",
+        duracion:153,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FP%C3%B3ster%20IG%20LIGHTYEAR.jpg?alt=media&token=b50949d9-820c-499d-9768-766dd6bce364",
+        sinopsis:"La historia del origen de Buzz Lightyear, el héroe que inspiró el juguete, y que nos da a conocer al legendario Guardián Espacial que acabaría contando con generaciones de fans.",
+        clasificacion:"SC",
+        idioma:"ESP",
+        director: "Angus MacLane",
+        actores: "Chris Evans, Taika Wakiti, Keke Palmer"
+
+    },
+    {
+        nombre:"Proximamente: Asesino sin memoria",
+        genero:"Acción, Suspenso",
+        duracion:193,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2Funnamed%20(21).jpg?alt=media&token=8e8c66df-157e-4db9-902a-c6224bd0adc5",
+        sinopsis:"Lex Lewis (Liam Neeson) es un asesino experto con una reputación de precisión discreta. Atrapado en un atolladero moral, Alex se niega a completar un trabajo que viola su código y debe cazar y matar rápidamente a las personas que lo contrataron antes de que ellos y el agente del FBI Vincent Serra (Guy Pearce) lo encuentren primero. Alex está hecho para la venganza pero, con una memoria que comienza a fallar, se ve obligado a cuestionar cada una de sus acciones, desdibujando la línea entre el bien y el mal.",
+        clasificacion:"B-15",
+        idioma:"EN/ESP Doblado",
+        director:"Martin Campbell",
+        actores: "Liam Neesson, Monica Belucci, Guy Pearce"
+    },
+    {
+        nombre:"Proximamente: Thor: Amor y Trueno",
+        genero:"Acción, Aventura",
+        duracion:145,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FThor-Amor-y-Trueno-Poster.jpg?alt=media&token=fc123e79-5d61-4f84-b0e9-9cdd237bba9b",
+        sinopsis:"La película encuentra a Thor (Chris Hemsworth) en un viaje diferente a todo lo que ha enfrentado: una búsqueda de paz interior. Pero su retiro es interrumpido por un asesino de la galaxia conocido como Gorr, el Carnicero de Dioses (Christian Bale), que busca la extinción de los dioses. Para combatir la amenaza, Thor solicita la ayuda del rey Valquiria (Tessa Thompson), Korg (Taika Waititi) y su ex novia Jane Foster (Natalie Portman), quien, para sorpresa de Thor, empuña inexplicablemente su martillo mágico, Mjolnir, como la Poderosa Thor. Juntos, se embarcan en una terrorífica aventura cósmica para descubrir el misterio de la venganza del Carnicero de Dioses y detenerlo antes de que sea demasiado tarde. Dirigida por Waititi (THOR: RAGNAROK, Jojo Rabbit) y producida por Kevin Feige y Brad Winderbaum.",
+        clasificacion:"C",
+        idioma:"EN Subtitulado",
+        director: "Taika Waititi",
+        actores:"Chris Hemsworth, Christian Bale, Tessa Thompson"
+
+    },
+    {
+        nombre:"Proximamente: Elvis",
+        genero:"Drama, Musical",
+        duracion:143,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2FFS-KJTUUYAAIUvF.jpg?alt=media&token=b27b879f-b96b-458b-8bf1-ce18dd12be7b",
+        sinopsis:"La película explora la vida y la música de Elvis Presley (Butler) a través del prisma de su complicada relación con el coronel Tom Parker (Hanks), su enigmático manager. La historia profundiza en la compleja dinámica que existía entre Presley y Parker que abarca más de 20 años, desde el ascenso de Presley a la fama hasta su estrellato sin precedentes, en el contexto de la revolución cultural y la pérdida de la inocencia en Estados Unidos. Y en el centro de ese periplo está Priscilla Presley (Olivia DeJonge), una de las personas más importantes e influyentes en la vida de Elvis.",
+        clasificacion:"PG-13",
+        idioma:"ESP Subtitulado",
+        director:"Baz Luhrmann",
+        actores:"Austin Buttler, Tom Hanks, Dacre Montgomery",
+
+    },
+
+    {
+        nombre:"Proximamente: Minions 2: Nace un villano",
+        genero:"Animada/Comedia",
+        duracion:189,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemr-464ee.appspot.com/o/Cineimg%2Fminions-nace-un-villano-poster-2.jpg?alt=media&token=4b8bcd0a-1c91-4f8e-ba0e-0acd5d96109e",
+        sinopsis:"Son los años 70, y el joven Gru crece en un barrio residencial de la ciudad, su sueño es ser un villano y por eso idolatra al grupo de supervillanos conocidos como Los Salvajes Seis. Para unirse a ellos, Gru idea un plan lo suficiente malvado con la ayuda de sus fieles seguidores, los Minions, siempre dispuestos a sembrar el caos. Junto con Kevin, Stuart, Bob y Otto, un nuevo minion con brackets y una desesperada necesidad de complacer al resto, desplegarán su potencial para construir su primera guarida, experimentar con sus primeras armas y llevar a cabo sus primeras misiones. Cuando Los Salvajes Seis deciden expulsar a su líder, el luchador de leyenda Nudillos Salvajes, Gru decide acudir a la entrevista para entrar como un nuevo miembro. Claro que nada acabará yendo bien en la entrevista y dará lugar a toda una aventura en la que Gru descubrirá que incluso los más malvados necesitan la ayuda de sus amigos.",
+        clasificacion:"SC",
+        idioma:"ESP Doblado",
+        director:"Heitor Pereira",
+        actores:"Seteve Carrel, Taraji Henson, Ruseel Brand",
+
+    },
 ]
+
+
 
 
 //RECORRIENDO UN ARREGLO EN JS
@@ -128,30 +218,58 @@ nombre.classList.add('card-title','text-center')
 nombre.textContent=pelicula.nombre
 
 //creamos el genero  de cada pelicula
-let genero = document.createElement('h4')
+let genero = document.createElement('h5')
 genero.classList.add('text-start')
 genero.textContent="Genero: "+pelicula.genero
 
 //creamos el idioma de cada pelicula
 let idioma=document.createElement('h6')
 idioma.classList.add('fw-bold')
-idioma.textContent=pelicula.idioma
+idioma.textContent="Idioma: "+pelicula.idioma
+
+//creamos la duracion
+let duracion = document.createElement('h6')
+duracion.classList.add('text-start')
+duracion.textContent="Duracion: "+pelicula.duracion
+
+//creamos la clasificacion
+let clasificacion = document.createElement('h6')
+clasificacion.classList.add('text-start')
+clasificacion.textContent="Clasificacion: "+pelicula.clasificacion
+
+//cramos los directores
+let director = document.createElement('h6')
+director.classList.add('text-start')
+director.textContent="Director: "+pelicula.director
+
+//creamos los actores
+let actores = document.createElement('h6')
+actores.classList.add('text-start')
+actores.textContent="Actores: "+pelicula.actores
+
 
 //creamos la sipnosis
-let sipnosis=document.createElement('p')
-sipnosis.classList.add('d-none')
-sipnosis.textContent=pelicula.sipnosis
+let sinopsis=document.createElement('h7')
+sinopsis.classList.add('text-start')
+sinopsis.textContent="Sipnosis: "+pelicula.sinopsis
+
 
 
 //Padres e hijos
 tarjeta.appendChild(poster)
 tarjeta.appendChild(nombre)
 tarjeta.appendChild(genero)
+tarjeta.appendChild(duracion)
 tarjeta.appendChild(idioma)
-tarjeta.appendChild(sipnosis)
+tarjeta.appendChild(clasificacion)
+tarjeta.appendChild(director)
+tarjeta.appendChild(actores)
+tarjeta.appendChild(sinopsis)
+
 
 column.appendChild(tarjeta)
 fila.appendChild(column)
+
 
 
 
@@ -160,6 +278,6 @@ fila.appendChild(column)
 
 //Detectando seleccion de una pelicula
 fila.addEventListener("click",function(){
-    alert("Estas seleccionand una pelicula")
+    alert("Estas seleccionando una pelicula")
 })
 
